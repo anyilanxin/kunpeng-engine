f56b25b2f8db33cfd93d92eab55bb904c2673d1e
xuanhong <62740686+zxuanhong@users.noreply.github.com>
2026-08-19 16:04:19 +0800

    refactor: keep only atomix and journal modules
    Changes:
    - Remove all modules except atomix and journal (zeebe-engine, broker,
      gateway, qa, benchmarks, CI scripts, etc.)
    - Remove code licensed under Zeebe Community License 1.1 from the
      atomix and journal modules:
      * atomix/cluster/.../raft/impl/zeebe/LogCompactor.java
      * atomix/cluster/.../raft/snapshot/SbeBufferWriterReader.java
      * atomix/cluster/src/test/.../RaftServerDisconnectTest.java
      * journal/src/test/.../file/LogCorrupter.java
    - All remaining code is licensed under Apache License 2.0
      (inherited from Camunda 8.0.4)
    
    See PROVENANCE.md and the initial commit message for the source baseline.

--- .ci/scripts/distribution/build-java.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -eux
-# Specifically for building, use as many CPUs as are available
-mvn -B -T1C -s "${MAVEN_SETTINGS_XML}" \
-  -DskipTests \
-  -Pspotbugs,prepare-offline -PcheckFormat,-autoFormat \
-  clean install
--- .ci/scripts/docker/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh -eux
-
-echo "Building Zeebe Docker image ${IMAGE}:${TAG}"
-docker build --no-cache --build-arg DISTBALL=camunda-zeebe.tar.gz -t ${IMAGE}:${TAG} --target app .
