62eb193fd58d097eca9067e9e9b935d8bf662081
xuanhong <62740686+zxuanhong@users.noreply.github.com>
2026-08-19 17:04:44 +0800

    chore: add copyright and license headers to build scripts and cluster sources
    - Append "Copyright © 2026 anyilanxin zxh(anyilanxin@aliyun.com)" below the
      upstream Copyright lines in all 411 Java files and logback.xml, keeping
      the original Apache 2.0 attribution intact
    - Add the AGPL 3.0 header to previously unlicensed files: log4j2-test.xml
      and the buildSrc/broker/cluster/gateway build scripts
    - Refresh the bom build scripts to MPL 2.0 with the 2026 copyright line

--- kunpeng/cluster/src/main/java/com/anyilanxin/kunpeng/cluster/primitive/partition/MemberGroupProvider.java
+++ kunpeng/cluster/src/main/java/com/anyilanxin/kunpeng/cluster/primitive/partition/MemberGroupProvider.java
@@ -1,6 +1,7 @@
 /*
  * Copyright 2018-present Open Networking Foundation
  * Copyright © 2020 camunda services GmbH (info@camunda.com)
+ * Copyright © 2026 anyilanxin zxh(anyilanxin@aliyun.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
